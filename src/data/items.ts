893cfe762a099193c055ab534551916d01299561
GabrielFayner <gabrielfayner10@gmail.com>
2022-01-27 11:37:30 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "McDonalds",
+    value: 24.5,
+  },
+  {
+    date: new Date(2021, 9, 8),
+    category: "food",
+    title: "Burguer king",
+    value: 28.99,
+  },
+  {
+    date: new Date(2021, 11, 25),
+    category: "rent",
+    title: "Aluguel Casa",
+    value: 1700,
+  },
+  {
+    date: new Date(2021, 10, 13),
+    category: "salary",
+    title: "Salário ACME",
+    value: 7800,
+  },
+];
